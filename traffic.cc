cb16823371883cfbb30afc30fc9fffe5c837faff
Florence-nyokabi <124134134+Florence-nyokabi@users.noreply.github.com>
2023-03-06 13:07:04 +0300

    traffic lights!

--- traffic.cc
+++ traffic.cc
@@ -2,6 +2,10 @@ int RED =1 ;
 int YELLOW = 2;
 int GREEN = 0;
 int Button = 3;
+int red = 8;
+int yellow = 9;
+int green = 10;
+int button = 11;
 void setup(){ pinMode(RED,OUTPUT);
               pinMode(YELLOW,OUTPUT);
               pinMode(GREEN,OUTPUT);
@@ -24,7 +28,7 @@ void loop(){
                     digitalWrite(green,HIGH);
                     delay(3000);
                     digitalWrite(RED,HIGH);
-                    digitalWrite(green,HIGH)
+                    digitalWrite(green,HIGH);
                     digitalWrite(YELLOW,HIGH);
                     digitalWrite(yellow, HIGH);
                     delay(2000);
@@ -38,10 +42,12 @@ void loop(){
                     digitalWrite(GREEN,HIGH);
                     digitalWrite(red, HIGH);
                     digitalWrite(YELLOW,HIGH);
+                    digitalWrite(yellow, HIGH);
                     delay(2000);
                     digitalWrite(GREEN,LOW);
                     digitalWrite(YELLOW,LOW);
-                   
-                    
-                  }
+                    digitalWrite(red, LOW);
+                    digitalWrite(YELLOW,HIGH);
+                    digitalWrite(yellow, HIGH);
+             }
 }
